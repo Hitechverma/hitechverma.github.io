a902150554d2874566883fe409baa587ab601a98
Hitech Verma <hitechlov22@gmail.com>
2015-02-22 15:37:04 +0530

    Admins updated

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
     var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
       center: new google.maps.LatLng(23.898688, 78.867790),
-      zoom: 8,
+      zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(mapCanvas, mapOptions);
